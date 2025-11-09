3bf5e22a1b3ed2a4966aba480fea5acac05552ee
Justin Nguyen <jooton11@gmail.com>
2025-11-08 20:21:17 -0700

    Push initial Java Spring Boot application to repo.

---
+++ pbswarn-alerts/src/main/java/com/SER401/pbswarn_alerts/PbswarnAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.SER401.pbswarn_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbswarnAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbswarnAlertsApplication.class, args);
+	}
+
+}
