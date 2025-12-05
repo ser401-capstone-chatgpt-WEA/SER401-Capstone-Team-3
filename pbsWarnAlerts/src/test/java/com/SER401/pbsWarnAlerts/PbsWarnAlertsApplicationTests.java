a663edcd416fd6a6231a3db5c4b384774ca01f48
Justin Nguyen <jooton11@gmail.com>
2025-12-05 15:55:13 -0700

    Push a new Java Spring Boot application to the repository because there were complications with the old one.

---
+++ pbsWarnAlerts/src/test/java/com/SER401/pbsWarnAlerts/PbsWarnAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SER401.pbsWarnAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbsWarnAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
